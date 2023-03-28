7079434aa0100afdddc7837f2a84968d984f8971
Michelle Huang <huang7ame@gmail.com>
2023-03-27 22:25:40 -0700

    App: introduce react-router

--- groove-app/src/App.js
+++ groove-app/src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Game from "./components/Game";
 import "./App.css";
 
 export default function App() {
 
   return (
-    <div className="App">
-      <Game/>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Game</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <Game />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
